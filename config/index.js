1d1ac3c49bb5698ada411b9ce9752fc8c73c9709
Andy Du Ly <andy.duly@benchedms.ca>
2021-03-02 01:47:12 -0500

    Init

--- config/index.js
+++ config/index.js
@@ -1,14 +1,13 @@
 module.exports = {
   //-- SITE SETTINGS -----
-  author: "@konstantinmuenster",
-  siteTitle: "Gatsby Starter Portfolio Minimal",
-  siteShortTitle: "Gatsby", // Used as logo text in header, footer, and splash screen
-  siteDescription:
-    "A modern one-page portfolio with a clean yet expressive design.",
+  author: "@andyduly",
+  siteTitle: "Andy Du Ly",
+  siteShortTitle: "AD", // Used as logo text in header, footer, and splash screen
+  siteDescription: "AD SPA Portfolio",
   siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
   siteLanguage: "en_US",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
-  seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  seoTitleSuffix: "Andy Du Ly", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
 
   // -- THEME SETTINGS -----
   colors: {
@@ -41,35 +40,30 @@ module.exports = {
     primary: "Roboto, Arial, sans-serif",
   },
 
-  //-- ARTICLES SECTION SETTINGS -----
-  // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
-  // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
-  mediumRssFeed:
-    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
-  // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
-
-  shownArticles: 3,
-
   //-- SOCIAL MEDIA SETTINGS -----
   // There are icons available for the following platforms:
   // Medium, GitHub, LinkedIn, XING, Behance
   socialMedia: [
     {
       name: "Linkedin",
-      url: "https://www.linkedin.com/in/konstantin-muenster/",
-    },
-    {
-      name: "Medium",
-      url: "https://konstantinmuenster.medium.com/",
+      url: "https://www.linkedin.com/in/andyduly/",
     },
     {
       name: "Github",
-      url:
-        "https://github.com/konstantinmuenster/gatsby-starter-portfolio-minimal",
+      url: "https://github.com/AndyDuLy",
     },
     {
-      name: "Behance",
-      url: "https://www.behance.net/konstanmnster",
+      name: "Medium",
+      url: "https://medium.com/@aandy.duly",
+    },
+    {
+      name: "Devpost",
+      url: "https://devpost.com/AndyDuLy",
+    },
+    {
+      name: "Resume",
+      url:
+        "https://drive.google.com/drive/folders/1apDueoMyihJ-xJa9baEI26n9imZ8QB8y?usp=sharing",
     },
   ],
 
@@ -77,15 +71,15 @@ module.exports = {
   navLinks: {
     menu: [
       {
-        name: "Articles",
-        url: "/#articles",
-      },
-      {
         name: "About Me",
         url: "/#about",
       },
       {
-        name: "Features",
+        name: "Skills",
+        url: "/#interests",
+      },
+      {
+        name: "Projects",
         url: "/#projects",
       },
     ],
@@ -94,14 +88,4 @@ module.exports = {
       url: "/#contact",
     },
   },
-  footerLinks: [
-    {
-      name: "Privacy",
-      url: "/privacy",
-    },
-    {
-      name: "Imprint",
-      url: "/imprint",
-    },
-  ],
 }
